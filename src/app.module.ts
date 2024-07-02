df5e4f5c8a3dfdfe0e0035deb16fde2bfd2b7a69
rodriguesrai <rairodrigues_sf@hotmail.com>
2024-07-02 17:37:22 -0300

    organize files of entities

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,15 +3,10 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './controllers/app.controller';
 import { AppService } from './services/app.service';
-import { CatsModule } from './cats.module';
 import { dataSourceOptions } from './database/config/ormconfig';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot(),
-    CatsModule,
-    TypeOrmModule.forRoot(dataSourceOptions),
-  ],
+  imports: [ConfigModule.forRoot(), TypeOrmModule.forRoot(dataSourceOptions)],
   controllers: [AppController],
   providers: [AppService],
 })
