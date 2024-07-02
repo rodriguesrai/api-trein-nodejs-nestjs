8e620113e26ddb2721860592b431a8b92edfa25e
rodriguesrai <rairodrigues_sf@hotmail.com>
2024-07-02 17:27:58 -0300

    refactoring TypeOrmModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,28 +1,16 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './controllers/app.controller';
 import { AppService } from './services/app.service';
 import { CatsModule } from './cats.module';
+import { dataSourceOptions } from './database/config/ormconfig';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     CatsModule,
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        type: (process.env.DATABASE_TYPE as any) ?? 'mysql',
-        host: configService.get<string>('DATABASE_HOST'),
-        port: configService.get<number>('DATABASE_PORT'),
-        username: configService.get<string>('DATABASE_USERNAME'),
-        password: configService.get<string>('DATABASE_PASSWORD'),
-        database: configService.get<string>('DATABASE_NAME'),
-        autoLoadEntities: true,
-        // synchronize: true, // Não use em produção
-      }),
-    }),
+    TypeOrmModule.forRoot(dataSourceOptions),
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/database/config/ormconfig.ts
+++ src/database/config/ormconfig.ts
@@ -1,5 +1,21 @@
-// import { DataSourceOptions, DataSource } from 'typeorm';
+import { DataSourceOptions, DataSource } from 'typeorm';
 
-// export const dataSourceOptions: DataSourceOptions = {
+export const dataSourceOptions: DataSourceOptions = {
+  type: (process.env.DATABASE_TYPE as any) ?? 'mysql',
+  host: process.env.DATABASE_HOST,
+  port: parseInt(process.env.DATABASE_PORT, 10) ?? 3306,
+  username: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+  synchronize: false,
+  bigNumberStrings: true,
+  multipleStatements: true,
+  logging: true,
+  entities: ['**/*.entity{ .ts,.js}'],
+  migrations: ['dist/database/migrations/*{.ts,.js}'],
+  migrationsRun: true,
+};
 
-// };
+const dataSource = new DataSource(dataSourceOptions);
+
+export default dataSource;
