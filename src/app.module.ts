b6523ba532b7e925911bf10a40c7b47111e99fd2
rodriguesrai <rairodrigues_sf@hotmail.com>
2024-07-08 11:45:28 -0300

    workflow 1.6 and typeormconfigs

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,25 +6,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { CatsModule } from './modules/cats.module';
 import { AuthModule } from './modules/auth.module';
 import { UsersModule } from './modules/users.module';
+import { dataSourceOptions } from './typeOrm.config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    TypeOrmModule.forRootAsync({
-      useFactory: () => ({
-        type: 'mysql',
-        host: process.env.DATABASE_HOST,
-        port: parseInt(process.env.DATABASE_PORT),
-        username: process.env.DATABASE_USERNAME,
-        password: process.env.DATABASE_PASSWORD,
-        database: process.env.DATABASE_NAME,
-        entities: [__dirname + '/**/*.entity{.ts,.js}'],
-        migrations: [__dirname + '/migrations/*{.ts,.js}'],
-        synchronize: true,
-      }),
-    }),
+    TypeOrmModule.forRoot(dataSourceOptions),
     CatsModule,
     AuthModule,
     UsersModule,
