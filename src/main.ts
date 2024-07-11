28842695ab1d97188e092caefd80f4c87515848a
rodriguesrai <rairodrigues_sf@hotmail.com>
2024-07-11 08:13:10 -0300

    change port api

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
 import { ValidationPipe } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+// import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // const configService = app.get(ConfigService);
+  // const port = configService.get<number>('API_PORT');
   app.useGlobalPipes(new ValidationPipe());
 
   const configSwagger = new DocumentBuilder()
@@ -17,6 +19,6 @@ async function bootstrap() {
   SwaggerModule.setup('api', app, document);
   const port = 3000;
   await app.listen(3000);
-  console.log(`Server running on http://localhost:${port}`);
+  console.log(`Server running on http://localhost:${port}!!`);
 }
 bootstrap();
