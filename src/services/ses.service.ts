639dbbee680de4a0950020c769a36796f1c55755
rodriguesrai <rairodrigues_sf@hotmail.com>
2024-07-12 16:17:41 -0300

    email sending successful

--- src/services/ses.service.ts
+++ src/services/ses.service.ts
@@ -6,28 +6,41 @@ import * as AWS from 'aws-sdk';
 @Injectable()
 export class SesService {
   private ses: AWS.SES;
-
+  private defaultSubject: string = 'Bem-vindo à nossa plataforma';
+  private defaultBody: string =
+    'Olá,\n\nBem-vindo à nossa plataforma! Esperamos que tenha uma excelente experiência.\n\nAtenciosamente,\nEquipe de Suporte';
   constructor(private configService: ConfigService) {
-    this.ses = new AWS.SES({
-      accessKeyId: this.configService.get<string>('AWS_ACCESS_KEY_ID'),
+    console.log(
+      'AWS_ACCESS_KEY_ID:',
+      this.configService.get<string>('AWS_ACCESS_KEY'),
+    );
+    console.log(
+      'AWS_SECRET_ACCESS_KEY:',
+      this.configService.get<string>('AWS_SECRET_ACCESS_KEY'),
+    );
+    console.log('AWS_REGION:', this.configService.get<string>('AWS_REGION'));
+
+    AWS.config.update({
+      accessKeyId: this.configService.get<string>('AWS_ACCESS_KEY'),
       secretAccessKey: this.configService.get<string>('AWS_SECRET_ACCESS_KEY'),
       region: this.configService.get<string>('AWS_REGION'),
     });
+    this.ses = new AWS.SES();
   }
 
-  async sendEmail(to: string, subject: string, body: string): Promise<void> {
+  async sendEmailCreatedUser(to: string): Promise<void> {
     const params = {
-      Source: 'rairodrigues@areopagus.tech',
+      Source: 'rairodrigues@areopagus.tech', // Email do remetente fixo
       Destination: {
-        ToAddresses: [to],
+        ToAddresses: [to], // Email do destinatário recebido como parâmetro
       },
       Message: {
         Subject: {
-          Data: subject,
+          Data: this.defaultSubject, // Assunto fixo
         },
         Body: {
           Text: {
-            Data: body,
+            Data: this.defaultBody, // Corpo do email fixo
           },
         },
       },
