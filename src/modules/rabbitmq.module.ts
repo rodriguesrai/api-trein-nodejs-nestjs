a7365235bbee0a442368e408ac0da57d8725a4ea
rodriguesrai <rairodrigues_sf@hotmail.com>
2024-07-15 15:56:49 -0300

    testing new version workflow

--- src/modules/rabbitmq.module.ts
+++ src/modules/rabbitmq.module.ts
@@ -11,7 +11,7 @@ import { EmailModule } from './email.module';
         name: 'RABBITMQ_SERVICE',
         transport: Transport.RMQ,
         options: {
-          urls: ['amqp://localhost:5672'],
+          urls: ['amqp://rabbitmq:5672'],
           queue: 'user_queue',
           queueOptions: {
             durable: true,
